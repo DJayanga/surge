36c071e0154d9bf19530e6eb835473a74d5c48c6
Dasun Jayanga <86801952+DJayanga@users.noreply.github.com>
2022-02-09 09:03:15 +0530

    Update AuthContext.js

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -8,9 +8,6 @@ function AuthContextProvider(props) {
 
   async function getLoggedIn() {
      const loggedInRes = await axios.get("http://localhost:5000/auth/loggedIn");
-    // const loggedInRes = await axios.get(
-    //   "https://mern-auth-template-tutorial.herokuapp.com/auth/loggedIn"
-    // );
     setLoggedIn(loggedInRes.data);
   }
 
